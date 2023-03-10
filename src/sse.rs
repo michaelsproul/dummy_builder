20ef1aa14a42f154a6233f5fb836f7ea29e8ffe2
Michael Sproul <micsproul@gmail.com>
2023-03-10 15:30:55 +1100

    Reconnect SSE on failure

--- src/sse.rs
+++ src/sse.rs
@@ -5,6 +5,7 @@ use futures::TryStreamExt;
 use lru::LruCache;
 use std::num::NonZeroUsize;
 use std::sync::Arc;
+use std::time::Duration;
 use tokio::sync::Mutex;
 
 #[derive(Debug, Clone)]
@@ -28,41 +29,46 @@ impl SseListener {
         tracing::info!("starting SSE listener");
 
         let url = format!("{}/eth/v1/events?topics=payload_attributes", self.url);
-        let client = ClientBuilder::for_url(&url).unwrap().build();
 
-        if let Err(e) = Box::pin(client.stream())
-            .try_for_each(|ev| async {
-                match ev {
-                    SSE::Event(event) => {
-                        let payload_attributes: VersionedSsePayloadAttributes =
-                            match serde_json::from_str(&event.data) {
-                                Ok(p) => p,
-                                Err(e) => {
-                                    tracing::error!("invalid payload attributes: {:?}", e);
-                                    return Ok(());
-                                }
-                            };
-                        let parent_block_hash = payload_attributes.data.parent_block_hash;
-                        let slot = payload_attributes.data.proposal_slot;
+        loop {
+            let client = ClientBuilder::for_url(&url).unwrap().build();
 
-                        tracing::info!(
-                            parent = ?parent_block_hash,
-                            slot = %slot,
-                            "got new payload attributes"
-                        );
+            if let Err(e) = Box::pin(client.stream())
+                .try_for_each(|ev| async {
+                    match ev {
+                        SSE::Event(event) => {
+                            let payload_attributes: VersionedSsePayloadAttributes =
+                                match serde_json::from_str(&event.data) {
+                                    Ok(p) => p,
+                                    Err(e) => {
+                                        tracing::error!("invalid payload attributes: {:?}", e);
+                                        return Ok(());
+                                    }
+                                };
+                            let parent_block_hash = payload_attributes.data.parent_block_hash;
+                            let slot = payload_attributes.data.proposal_slot;
 
-                        self.payload_attributes
-                            .lock()
-                            .await
-                            .put((parent_block_hash, slot), payload_attributes);
-                        Ok(())
+                            tracing::info!(
+                                parent = ?parent_block_hash,
+                                slot = %slot,
+                                "got new payload attributes"
+                            );
+
+                            self.payload_attributes
+                                .lock()
+                                .await
+                                .put((parent_block_hash, slot), payload_attributes);
+                            Ok(())
+                        }
+                        SSE::Comment(_) => Ok(()),
                     }
-                    SSE::Comment(_) => Ok(()),
-                }
-            })
-            .await
-        {
-            tracing::error!("stream terminated with error: {:?}", e);
+                })
+                .await
+            {
+                // Wait a while before attempting to reconnect.
+                tracing::error!(error = ?e, "stream disconnected. reconnecting in 10s");
+                tokio::time::sleep(Duration::from_secs(10)).await;
+            }
         }
     }
 
